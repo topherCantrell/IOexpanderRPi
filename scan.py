d3613ea12c3128b75aefd7cf1768c1698cae72ed
Christopher Cantrell <topherCantrell@gmail.com>
2018-10-19 10:54:19 -0500

    Update scan.py

--- scan.py
+++ scan.py
@@ -1,5 +1,34 @@
+import time
 
 def set_line_input(x):
+    # Pull down resistor
+    # TODO I2C communication here
     pass
     
+def set_line_output(x):
+    # Drive line high
+    # TODO I2C communication here
+    pass
+
+def is_line_high(x):
+    # Return true if line is high
+    # TODO I2C communication here
+    return False
+
+def sweep():
+    # Run the grid and see what lines are shorted together
+    for x in range(32):
+        set_line_output(x)
+        time.sleep(0.01)
+        for y in range(32):
+            if y!=x:
+                if is_line_high(y):
+                    print('(%d,%d)' % (x,y),end='')
+        set_line_input(x)
+        time.sleep(0.01)    
     
+for x in range(32):
+    set_line_input(x)
+    
+while True:
+    sweep()
